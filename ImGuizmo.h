67286b86e91fd84a1814b877be0e44f310f77c15
Giorgio Gori <gori@adobe.com>
2020-09-22 14:05:47 -0700

    added API support for drawing gizmo on any ImDrawList

--- ImGuizmo.h
+++ ImGuizmo.h
@@ -115,6 +115,8 @@ namespace ImGuizmo
 {
 	// call inside your own window and before Manipulate() in order to draw gizmo to that window.
 	IMGUI_API void SetDrawlist();
+	// Or pass a specific ImDrawList to draw to (e.g. ImGui::GetForegroundDrawList()).
+	IMGUI_API void SetDrawlist(ImDrawList* drawlist = nullptr);
 
 	// call BeginFrame right after ImGui_XXXX_NewFrame();
 	IMGUI_API void BeginFrame();
