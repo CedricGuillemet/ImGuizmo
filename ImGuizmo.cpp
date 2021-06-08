968a73cd2ea2b4b00d20eb51efab399f8789c4a9
Cooper <Aaron.Cooper@lith.com>
2021-06-08 16:04:43 -0700

    Windows unfortunately defines near and far in windef.h. This caused a symbol conflict. Renamed near and far in ImGuizmo.cpp to nearPos and farPos.

--- ImGuizmo.cpp
+++ ImGuizmo.cpp
@@ -1002,11 +1002,11 @@ namespace ImGuizmo
       gContext.mCameraUp = viewInverse.v.up;
 
       // projection reverse
-       vec_t near, far;
-       near.Transform(makeVect(0, 0, 1.f, 1.f), gContext.mProjectionMat);
-       far.Transform(makeVect(0, 0, 2.f, 1.f), gContext.mProjectionMat);
+       vec_t nearPos, farPos;
+       nearPos.Transform(makeVect(0, 0, 1.f, 1.f), gContext.mProjectionMat);
+       farPos.Transform(makeVect(0, 0, 2.f, 1.f), gContext.mProjectionMat);
 
-       gContext.mReversed = (near.z/near.w) > (far.z / far.w);
+       gContext.mReversed = (nearPos.z/nearPos.w) > (farPos.z / farPos.w);
 
       // compute scale from the size of camera right vector projected on screen at the matrix position
       vec_t pointRight = viewInverse.v.right;
