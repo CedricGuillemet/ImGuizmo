99876c31a6bf4da430bafc465f38c797c12bdefd
Khaled Abdsalame <154564727+Khaledabdsalame@users.noreply.github.com>
2025-03-28 02:47:13 +0100

    Update ImGuizmo.cpp
    Enabled White Dot By Default

--- ImGuizmo.cpp
+++ ImGuizmo.cpp
@@ -44,7 +44,8 @@
 
 namespace IMGUIZMO_NAMESPACE
 {
-   static bool drawCircle = false;
+//Disabled White Dot By Default
+   static bool drawCircle = true;
    static const float ZPI = 3.14159265358979323846f;
    static const float RAD2DEG = (180.f / ZPI);
    static const float DEG2RAD = (ZPI / 180.f);
